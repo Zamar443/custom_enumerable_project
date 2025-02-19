f72881377191116b512d9fe567cccd8c730661e7
zamar443 <165084526+zamar443@users.noreply.github.com>
2025-02-19 13:02:55 +0000

    add

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,8 +1,24 @@
 module Enumerable
-  # Your code goes here
+  def my_all?(&block)
+    block ||= proc { |value| value } # Default block for truthy check
+    each { |value| return false unless block.call(value) }
+    true
+  end
+end
+class Array
+  include Enumerable
+end
+
+module Enumerable
+  def my_each
+    for i in 0...self.length
+      yield(self[i])
+    end
+  end
 end
 
 # You will first have to define my_each
+
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
